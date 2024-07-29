3827fe64ad7037b2f1c6f85872f1c578008e1e76
gusgh3 <poweremperor@naver.com>
2024-07-29 14:44:19 +0900

    update 기능구현

--- crud/urls.py
+++ crud/urls.py
@@ -29,5 +29,9 @@ urlpatterns = [
 
     # Delete
     path('posts/<int:id>/delete/', views.delete), 
+
+    #update
+    path('posts/<int:id>/edit/', views.edit),
+    path('posts/<int:id>/update/', views.update),
    
 ]
